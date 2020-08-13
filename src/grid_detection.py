f3ee230a426f3d3ad2bc52733c5de004ce8400d1
doguscank <doguscan.k@hotmail.com>
2020-08-13 21:49:02 +0300

    getting grid image

---
+++ src/grid_detection.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+import itertools
+
+cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+
+def DetectGrid(img):
+	img = cv2.resize(img, (512, 512))
+
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	blur = cv2.GaussianBlur(gray, (5, 5), 0)
+	thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 5, 2)
+	edges = cv2.Canny(blur, 75, 150)
+
+	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	max_contour_area = 1000
+	best_contour = None
+
+	for contour in contours:
+		area = cv2.contourArea(contour)
+
+		if area > max_contour_area:
+			max_contour_area = area
+			best_contour = contour
+	
+	approx_poly = cv2.approxPolyDP(best_contour, 1, True)
+	approx_poly = np.array([a[0] for a in approx_poly])
+
+	combinations = np.array(list(itertools.combinations(approx_poly, 4)))
+
+	best_combination = None
+	max_area = 1000
+
+	for c in combinations:
+		area = cv2.contourArea(c)
+
+		if area > max_area:
+			max_area = area
+			best_combination = c
+
+	#print(best_combination)
+
+	x_sorted = sorted(best_combination, key = lambda x: x[0])
+	#print(x_sorted)
+
+	p_1 = min(x_sorted[:2], key = lambda x: x[1])
+	p_3 = max(x_sorted[:2], key = lambda x: x[1])
+	p_2 = min(x_sorted[2:], key = lambda x: x[1])
+	p_4 = max(x_sorted[2:], key = lambda x: x[1])
+	#print(p_1, p_2, p_3, p_4)
+
+	pts_1 = np.float32([p_1, p_2, p_3, p_4])
+	print(pts_1)
+
+	pts_2 = np.float32([[0, 0], [512, 0], [0, 512], [512, 512]])
+	print(pts_2)
+
+	mask = np.zeros(gray.shape, np.uint8)
+	cv2.drawContours(mask, [best_combination], -1, 255, -1)
+	cv2.drawContours(mask, [best_combination], 0, 0, 2)
+
+	masked_img = cv2.bitwise_and(img, img, mask = mask)
+
+	#for a in best_combination:
+	#	cv2.circle(masked_img, (a[0], a[1]), 3, (0, 0, 255), -1)
+	#	cv2.putText(masked_img, "{}, {}".format(a[0], a[1]), (a[0], a[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0))
+
+	M = cv2.getPerspectiveTransform(pts_1, pts_2)
+	dst = cv2.warpPerspective(masked_img, M, (512, 512))
+
+	return dst
+
+#Testing the functions here
+if __name__ == '__main__':
+	img = cv2.imread('./images/sudoku_newspaper_3.jpg')
+	img = DetectGrid(img)
+	cv2.imshow('test', img)
+	cv2.waitKey(0)
